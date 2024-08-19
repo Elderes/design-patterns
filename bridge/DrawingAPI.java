7a6a1b8570a7999864cf22a5d6c04c1106915543
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-08-19 11:56:27 -0300

    padrão bridge não finalizado

---
+++ bridge/DrawingAPI.java
@@ -0,0 +1,7 @@
+package bridge;
+
+// Implementador 
+public interface DrawingAPI {
+    void drawCircle(double x, double y, double radius);
+    void drawSquare(double x, double y, double side);
+}
